b0895b410fba919c7679acddc44ab6f2f2c95547
ericagaffud <ericadurangaffud@gmail.com>
2024-06-05 14:22:03 +0800

    shorten conditions

--- for_loop.py
+++ for_loop.py
@@ -21,11 +21,13 @@ submit.click()
 driver.get("http://binpo.paybps.ovpn/intranet/panels/helpdesk_edit_rules.php?id=110")
 
 names = ["A", "B", "C"]
+max_values = {"A": 5, "B": 5, "C": 6}
 value_opt = [1, 2, 3]
 
 for x, y in zip(names, value_opt):
-    if x == "A" and y == 1 or x == "B" and y == 2:
-        for a in range(1,2):
+    if x == "A" and y == 1 or x == "B" and y == 2 or x == "C" and y == 3:
+        max_value = max_values[x]
+        for a in range(1, max_value + 1):
             #Add New Condition Set Button
             driver.find_element(By.NAME, "add_rule_link").click()
             #Condition Name
@@ -56,37 +58,4 @@ for x, y in zip(names, value_opt):
             #Sumbit
             driver.find_element(By.XPATH, submit_button).click()
             time.sleep(2)
-            
-    elif x == "C" and y == 3:
-        for a in range(1,2):
-            #Add New Condition Set Button
-            driver.find_element(By.NAME, "add_rule_link").click()
-            #Condition Name
-            condition_name = driver.find_element(By.ID, "rule_name")
-            condition_name.send_keys(f"Actions#7__{x}{a}") 
-            #Three Checkbox
-            driver.find_element(By.ID, "for_fields_rights").click()
-            driver.find_element(By.ID, "for_workflow").click()
-            driver.find_element(By.ID, "for_sla").click()
-            time.sleep(2)
-            
-            #Content
-            threats_xpath = "//input[@value='threats_that_can_lead_to__7']"
-            threats_value = f"{threats_xpath}//ancestor::tr//select[@class='form-control']/option[{y}]"
-            supporting_xpath = "//input[@value='supporting_assets_7']"
-            supporting_value = f"{supporting_xpath}//ancestor::tr//select[@class='form-control']/option[{a}]"
-            submit_button = "//input[@value='Save']"
-
-            #Threats that leads to
-            driver.find_element(By.XPATH, threats_xpath).click()
-            driver.find_element(By.XPATH, threats_value).click()
-            time.sleep(2)
-            #Supporting Assets
-            driver.find_element(By.XPATH, supporting_xpath).click()
-            driver.find_element(By.XPATH, supporting_value).click()
-            time.sleep(2)
-            #Sumbit
-            driver.find_element(By.XPATH, submit_button).click()
-            time.sleep(2)
-
 input()
