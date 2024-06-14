baa0e8d52a03aac74f690434a8de63b3cf9b6c25
ericagaffud <ericadurangaffud@gmail.com>
2024-06-14 14:15:10 +0800

    initial commit of workflow

--- actions-workflow.py
+++ actions-workflow.py
@@ -22,7 +22,6 @@ submit.click()
 
 driver.get("http://binpo.paybps.ovpn/intranet/panels/helpdesk_edit_workflow.php?id=110")
 
-
 #Select Field
 driver.find_element(By.ID, "new_field_symname").click()
 option_id = "//option[@value='actions_7']"
@@ -32,9 +31,6 @@ original_window = driver.current_window_handle
 #Add Rule For This Field
 driver.find_element(By.XPATH, "//input[@type='submit']").click()
 
-names = ["A", "E", "X"]
-max_values = {"A": 3, "B": 5, "C": 6}
-
 for window_handle in driver.window_handles:
     if window_handle != original_window:
         new_window = window_handle
@@ -43,54 +39,68 @@ for window_handle in driver.window_handles:
 driver.switch_to.window(new_window)
 time.sleep(2)
 
+names = ["A", "E", "X"]
+iterations = [1, 2, 3]
+max_values = {"A": 2, "B": 2, "C": 2}
 #Start Loop
-for x in names:
-    if x == 'A' or x == 'B' or x == 'C':
-        max_value = max_values[x]
-        for a in range(1, max_value + 1):
+for name in names:
+    if name == 'A' or name == 'B' or name == 'C':
+        max_value = max_values[name]
+        for iterations in range(1, max_value + 1):
             #Field Condition Name
             condition_select = "//select[@name='rule_id']"
             driver.find_element(By.XPATH, condition_select).click()
             time.sleep(2)
-            condition_element = f"//select[@name='rule_id']/option[text()='Actions#7__{x}{a}']"
+            condition_element = f"//select[@name='rule_id']/option[text()='Actions#7__{name}{iterations}']"
             driver.find_element(By.XPATH, condition_element).click()
             time.sleep(2)
-            
-            for a in range(1, 3):
+
+            number_of_role = 2
+            for role in range(1, number_of_role + 1):
                 #Role Name
                 driver.find_element(By.ID, "roleList").click()
-                role_option = f"//select[@id='roleList']/option[{a}]"
+                role_option = f"//select[@id='roleList']/option[{role}]"
                 driver.find_element(By.XPATH, role_option).click()
                 time.sleep(2)
-                if(a == 1):
-                    #checkbox
+
+                option1_user = driver.find_element(By.XPATH, "//select[@id='options_select397']/option[1]") #Used inappropriately
+                option2_user = driver.find_element(By.XPATH, "//select[@id='options_select397']/option[2]") #Observed
+                option3_user = driver.find_element(By.XPATH, "//select[@id='options_select397']/option[3]") #Altered
+                option4_user = driver.find_element(By.XPATH, "//select[@id='options_select397']/option[4]") #Lost
+                option5_user = driver.find_element(By.XPATH, "//select[@id='options_select397']/option[5]") #Manipulated
+                option6_user = driver.find_element(By.XPATH, "//select[@id='options_select397']/option[6]") #Overloaded
+                option7_user = driver.find_element(By.XPATH, "//select[@id='options_select397']/option[7]") #Damaged
+
+                option1_tm = driver.find_element(By.XPATH, "//select[@id='options_select398']/option[1]") #Used inappropriately
+                option2_tm = driver.find_element(By.XPATH, "//select[@id='options_select398']/option[2]") #Observed
+                option3_tm = driver.find_element(By.XPATH, "//select[@id='options_select398']/option[3]") #Altered
+                option4_tm = driver.find_element(By.XPATH, "//select[@id='options_select398']/option[4]") #Lost
+                option5_tm = driver.find_element(By.XPATH, "//select[@id='options_select398']/option[5]") #Manipulated
+                option6_tm = driver.find_element(By.XPATH, "//select[@id='options_select398']/option[6]") #Overloaded
+                option7_tm = driver.find_element(By.XPATH, "//select[@id='options_select398']/option[7]") #Damaged
+                if(name == "A" and iterations == 1 or role == 1):
                     driver.find_element(By.NAME, "not_used_checkbox397").click()
-                    #select
-                    fields_value1 = "//select[@id='options_select397']/option[1]"
-                    fields_value2 = "//select[@id='options_select397']/option[2]"
-                    option1 = driver.find_element(By.XPATH, fields_value1)
-                    option2 = driver.find_element(By.XPATH, fields_value2)
+                    action = ActionChains(driver)
+                    action.key_down(Keys.CONTROL).click(option1_user).click(option2_user).click(option3_user).click(option4_user).key_up(Keys.CONTROL).perform()
                 else:
-                    #checkbox
-                    driver.find_element(By.NAME, "not_used_checkbox398").click() 
-                    #select
-                    fields_value1 = "//select[@id='options_select398']/option[1]"
-                    fields_value2 = "//select[@id='options_select398']/option[2]"
-                    option1 = driver.find_element(By.XPATH, fields_value1)
-                    option2 = driver.find_element(By.XPATH, fields_value2)
-
-                time.sleep(2)
-                #Click ctrl key
-                action = ActionChains(driver)
-                action.key_down(Keys.CONTROL).click(option1).click(option2).key_up(Keys.CONTROL).perform()
-                time.sleep(2)
-
+                    driver.find_element(By.NAME, "not_used_checkbox398").click()
+                    action = ActionChains(driver)
+                    action.key_down(Keys.CONTROL).click(option1_tm).click(option2_tm).click(option3_tm).click(option4_tm).key_up(Keys.CONTROL).perform()
+                    time.sleep(2)
             #Add/Change Rule
             submit_button = "//input[@type='submit']"
             driver.find_element(By.XPATH, submit_button).click()
             time.sleep(5)
+            driver.switch_to.window(original_window)
+            original_window = driver.current_window_handle
             #Add New Rule
             new_rule = "//td/span[text()='actions_7']/ancestor::tr/following-sibling::tr//div[@id='new_link']/a[1]"
             driver.find_element(By.XPATH, new_rule).click()
+
+            for window_handle in driver.window_handles:
+                if window_handle != original_window:
+                    new_window = window_handle
+                    break
+            driver.switch_to.window(new_window)
             #End Loop
 input()
