36b62f8cc8d3c08523c8ce6eb73485d03409097a
marhano <carullomarjan@gmail.com>
2024-06-24 18:44:56 +0800

    .remove creden

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from infographics.infographics import Infographics
     
 if __name__ == "__main__":
     with Infographics() as bot:
-        bot.login_user(username="binpo.mbcarullo", password="566bXeYDr4")
+        bot.login_user(username="", password="")
         bot.ob_class_b_pas()
        
